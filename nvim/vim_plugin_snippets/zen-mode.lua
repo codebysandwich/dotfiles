c77a3e98959d2c887f8ebebdb0c2a087be75f829
sandwich <122079260@qq.com>
2022-11-11 21:54:29 +0800

    update

--- nvim/vim_plugin_snippets/zen-mode.lua
+++ nvim/vim_plugin_snippets/zen-mode.lua
@@ -1,13 +1,13 @@
 require("zen-mode").setup {
     window = {
-  	backdrop = 1,
-  	width = 1, 
-  	height = 1,
+		backdrop = 1,
+		width = 1, 
+		height = 1,
     },
     plugins = {
-  	options = {
-  	  showcmd = false,
-  	},
-    -- tmux = { enabled = true },
+		options = {
+			showcmd = false,
+		},
+		-- tmux = { enabled = true },
     }
 }
